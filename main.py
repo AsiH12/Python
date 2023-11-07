958d9ed6c57149d305261a4a6ac7278eed1cc432
Asi Hassan <142927358+AsiH12@users.noreply.github.com>
2023-11-07 18:37:29 +0200

    TicTacToe

--- main.py
+++ main.py
@@ -1,51 +1,149 @@
 import json
-import random
 
 
-def display_score(score):
-    print("Your score is now:", score)
-    if score == 0:
-        print("This is not great!")
+def display_board(board):
+    board_format = """
+    A | B | C
+    -----------
+      |   |
+ 1| {} | {} | {}
+      |   |
+    -----------
+      |   |
+ 2| {} | {} | {}
+      |   |
+    -----------
+      |   |
+ 3| {} | {} | {}"""
+    print(
+        board_format.format(
+            board[0][0],
+            board[0][1],
+            board[0][2],
+            board[1][0],
+            board[1][1],
+            board[1][2],
+            board[2][0],
+            board[2][1],
+            board[2][2],
+        )
+    )
 
 
-def check_answer(possible_answers, score):
-    user_answer = input("Your answer: ")
-    exit_words = ["exit", "quit", "q", "x"]
-    if user_answer.lower() in exit_words:
-        exit()
-    if user_answer.lower() in possible_answers:
-        score = score + 1
-        print("Correct!")
+def check_win(board):
+    x_wins = ["X", "X", "X"]
+    o_wins = ["O", "O", "O"]
+
+    for i in range(3):
+        column = [board[0][i], board[1][i], board[2][i]]
+        row = [board[i][0], board[i][1], board[i][2]]
+        if column in [x_wins, o_wins] or row in [x_wins, o_wins]:
+            return True
+
+    if board[0][0] == board[1][1] == board[2][2] != " ":
+        return True
+    if board[0][2] == board[1][1] == board[2][0] != " ":
+        return True
+
+    # win_combos = [  # These are the indexes of all the possible ways to win
+    #     [0, 1, 2],  # Top row
+    #     [3, 4, 5],  # Middle row
+    #     [6, 7, 8],  # Bottom row
+    #     [0, 3, 6],  # Left column
+    #     [1, 4, 7],  # Middle column
+    #     [2, 5, 8],  # Right column
+    #     [0, 4, 8],  # Diagonal (top left to bottom right)
+    #     [2, 4, 6],  # Diagonal (top right to bottom left)
+    # ]
+    # for combo in win_combos:
+    #     if board[combo[0]] == board[combo[1]] == board[combo[2]] != " ":
+    #         return True
+    # return False
+
+
+def check_tie(board):
+    if not check_win(board) and " " not in board:
+        return True
     else:
-        print("Incorrect!")
-    return score
+        return False
 
 
-def ask_question(question_object, current_score):
-    print(question_object["text"])
-    if "wrong" in question_object.keys():  # We will only display multiple choice if there are wrong answers
-        letters = "ABCDEFG"  # This is a string, so we can iterate over it to get A, B, C, etc.
-        options = question_object["wrong"] + [question_object["answer"][0]]  # Add the correct answer to the list of wrong answers
-        random.shuffle(options)  # Shuffle the options so the correct answer isn't always the last one
-        for item in zip(letters, options):  # zip() lets us iterate over two lists at the same time, an `item` will be a tuple of (letter, answer)
-            letter = item[0]  # This is the letter, like "A"
-            option = item[1]  # This is one of the possible options, like "Paris"
-            if (option.lower() in question_object["answer"]):  # If this is the correct answer...
-                question_object["answer"].append(letter.lower())  # ...add the letter to the list of possible answers
-            option = "{}. {}".format(letter, option.capitalize())  # This will format the option to be "A. Option"
-            print(option)
-    score = check_answer(question_object["answer"], current_score)
-    display_score(score)
-    return score
+def save_results(winner):
+    try:
+        results_file = open("results.json", "r+")
+    except FileNotFoundError:
+        results_file = open("results.json", "w")
+        if winner == "Tie":
+            results = {"X": 0, "O": 0, "Tie": 1}
+        elif winner == "X":
+            results = {"X": 1, "O": 0, "Tie": 0}
+        elif winner == "O":
+            results = {"X": 0, "O": 1, "Tie": 0}
+        json.dump(results, results_file)
+        results_file.close()
+    else:
+        results = json.load(results_file)
+        if winner == "Tie":
+            results["Tie"] = results["Tie"] + 1
+        elif winner == "X":
+            results["X"] = results["X"] + 1
+        elif winner == "O":
+            results["O"] = results["O"] + 1
+        results_file.seek(0)
+        json.dump(results, results_file)
+        results_file.close()
+    print(
+        "The current results are: X: {}, O: {}, Tie: {}".format(
+            results["X"], results["O"], results["Tie"]
+        )
+    )
 
 
-questions_file = open("questions.json", "r")
-questions = json.load(questions_file)
-questions_file.close()
+winner = False
+board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
-score = 0
-random.shuffle(questions)  # Shuffle the questions so they aren't always in the same order
-for question in questions:
-    score = ask_question(question, score)
 
-print("Game over, thanks for playing!")
+current_player = "X"
+while not winner:
+    display_board(board)
+    print("It is {}'s turn.".format(current_player))
+    move = input("Where would you like to move? ")
+    move = move.lower()
+    if move in ["exit", "quit", "q", "x"]:
+        print("Stopping game... Goodbye!")
+        exit()
+
+    columns = {"a": 0, "b": 1, "c": 2}
+    rows = {"1": 0, "2": 1, "3": 2}
+
+    for letter in move:
+        if letter in columns.keys():
+            column = columns[letter]
+    for letter in move:
+        if letter in rows.keys():
+            row = rows[letter]
+    try:
+        if board[row][column] != " ":
+            # print("That is not a valid move.")
+            # continue
+            raise NameError
+        board[row][column] = current_player
+    except NameError:
+        print("That is not a valid move.")
+        continue
+
+    if check_win(board):
+        winner = current_player
+    # elif check_tie(board):
+    #     winner = "Tie"
+    if current_player == "X":
+        current_player = "O"
+    else:
+        current_player = "X"
+
+display_board(board)
+if winner == "Tie":
+    print("It's a tie!")
+else:
+    print("Congratulations, {} wins!".format(winner))
+save_results(winner)
--- with_class.py
+++ with_class.py
@@ -1,22 +1,134 @@
-from cards import create_deck, deal_cards
-from checks import check_hand
+import json
 
-class CardGame:
-    def __init__(self, num_players, num_cards_per_hand):
-        self.deck = create_deck()
-        self.num_players = num_players
-        self.num_cards_per_hand = num_cards_per_hand
+class TicTacToe:
+    def __init__(self):
+        self.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        self.current_player = "X"
+        self.winner = None
 
-    def play_game(self):
-        for i in range(self.num_players):
-            player_hand = deal_cards(self.deck, self.num_cards_per_hand)
-            hand_result = check_hand(player_hand)
-            print(f"Player {i + 1} Hand: {player_hand}, Result: {hand_result}")
+    def display_board(self):
+        board_format = """
+        A | B | C
+        -----------
+          |   |
+     1| {} | {} | {}
+          |   |
+        -----------
+          |   |
+     2| {} | {} | {}
+          |   |
+        -----------
+          |   |
+     3| {} | {} | {}"""
+        print(
+            board_format.format(
+                self.board[0][0],
+                self.board[0][1],
+                self.board[0][2],
+                self.board[1][0],
+                self.board[1][1],
+                self.board[1][2],
+                self.board[2][0],
+                self.board[2][1],
+                self.board[2][2],
+            )
+        )
 
-        print(f"Remaining cards in the deck: {len(self.deck)}")
+    def check_win(self):
+        x_wins = ["X", "X", "X"]
+        o_wins = ["O", "O", "O"]
+
+        for i in range(3):
+            column = [self.board[0][i], self.board[1][i], self.board[2][i]]
+            row = [self.board[i][0], self.board[i][1], self.board[i][2]]
+            if column in [x_wins, o_wins] or row in [x_wins, o_wins]:
+                return True
+
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != " ":
+            return True
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != " ":
+            return True
+
+    def check_tie(self):
+        if not self.check_win() and " " not in self.board:
+            return True
+        else:
+            return False
+
+    def save_results(self):
+        try:
+            results_file = open("results.json", "r+")
+        except FileNotFoundError:
+            results_file = open("results.json", "w")
+            if self.winner == "Tie":
+                results = {"X": 0, "O": 0, "Tie": 1}
+            elif self.winner == "X":
+                results = {"X": 1, "O": 0, "Tie": 0}
+            elif self.winner == "O":
+                results = {"X": 0, "O": 1, "Tie": 0}
+            json.dump(results, results_file)
+            results_file.close()
+        else:
+            results = json.load(results_file)
+            if self.winner == "Tie":
+                results["Tie"] = results["Tie"] + 1
+            elif self.winner == "X":
+                results["X"] = results["X"] + 1
+            elif self.winner == "O":
+                results["O"] = results["O"] + 1
+            results_file.seek(0)
+            json.dump(results, results_file)
+            results_file.close()
+        print(
+            "The current results are: X: {}, O: {}, Tie: {}".format(
+                results["X"], results["O"], results["Tie"]
+            )
+        )
+
+    def play(self):
+        while not self.winner:
+            self.display_board()
+            print("It is {}'s turn.".format(self.current_player))
+            move = input("Where would you like to move? ")
+            move = move.lower()
+            if move in ["exit", "quit", "q", "x"]:
+                print("Stopping game... Goodbye!")
+                exit()
+
+            columns = {"a": 0, "b": 1, "c": 2}
+            rows = {"1": 0, "2": 1, "3": 2}
+
+            column, row = None, None
+            for letter in move:
+                if letter in columns.keys():
+                    column = columns[letter]
+                if letter in rows.keys():
+                    row = rows[letter]
+
+            try:
+                if self.board[row][column] != " ":
+                    print("That is not a valid move.")
+                    continue
+                self.board[row][column] = self.current_player
+            except (TypeError, IndexError):
+                print("That is not a valid move.")
+                continue
+
+            if self.check_win():
+                self.winner = self.current_player
+
+            if self.current_player == "X":
+                self.current_player = "O"
+            else:
+                self.current_player = "X"
+
+        self.display_board()
+        if self.winner == "Tie":
+            print("It's a tie!")
+        else:
+            print("Congratulations, {} wins!".format(self.winner))
+        self.save_results()
 
 if __name__ == "__main__":
-    num_players = 5
-    num_cards_per_hand = 5
-    game = CardGame(num_players, num_cards_per_hand)
-    game.play_game()
+    game = TicTacToe()
+    game.play()
